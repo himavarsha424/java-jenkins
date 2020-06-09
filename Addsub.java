d8fda61a43bde1510bb9bbc3559c5c58613c25cc
himavarsha424 <60322991+himavarsha424@users.noreply.github.com>
2020-06-09 18:16:58 -0400

    Add files via upload

---
+++ Addsub.java
@@ -0,0 +1,16 @@
+
+
+public class Addsub
+{
+	public static void main(String[] args)
+	{
+			
+int a=10;
+int b=20;
+System.out.println("Enter Two Numbers:" + a +" " + b);
+int sum = a+b;
+int sub = b-a;
+System.out.println(  "The sum is:" +sum);
+System.out.println( "The sub is:" +sub);
+	}
+}
